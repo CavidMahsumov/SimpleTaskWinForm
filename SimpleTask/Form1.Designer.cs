319543621fad2f63446f4a8abcaaf0d57c5ae43f
Cavid <mehsumovcavid@gmail.com>
2021-06-03 21:04:31 +0400

    Add project files.

---
+++ SimpleTask/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SimpleTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LondonBtn = new System.Windows.Forms.Button();
+            this.Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(56, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Baku";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LondonBtn
+            // 
+            this.LondonBtn.AutoSize = true;
+            this.LondonBtn.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.LondonBtn.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LondonBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.LondonBtn.Location = new System.Drawing.Point(734, 61);
+            this.LondonBtn.Name = "LondonBtn";
+            this.LondonBtn.Size = new System.Drawing.Size(112, 52);
+            this.LondonBtn.TabIndex = 1;
+            this.LondonBtn.Text = "London";
+            this.LondonBtn.UseVisualStyleBackColor = false;
+            this.LondonBtn.Click += new System.EventHandler(this.LondonBtn_Click);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Label.Location = new System.Drawing.Point(378, 18);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(81, 30);
+            this.Label.TabIndex = 2;
+            this.Label.Text = "CLOCK";
+            this.Label.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::SimpleTask.Properties.Resources._49e9a026fa1aceaf5988c572a8f3f7fd;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.LondonBtn);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LondonBtn;
+        private System.Windows.Forms.Label Label;
+    }
+}
+
---
+++ SimpleTask/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleTask
+{
+    public partial class Form1 : Form
+    {
+        Timer Timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = 500;
+            Timer.Start();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources._49e9a026fa1aceaf5988c572a8f3f7fd;
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = 500;
+            Timer.Start();
+ 
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Label.Text = DateTime.Now.ToLongTimeString();
+        }
+        private void Timer_TickForLondon(object sender, EventArgs e)
+        {
+            Label.Text = DateTime.Now.AddHours(4).ToLongTimeString();
+        }
+
+        private void LondonBtn_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.london;
+            var datetimeforLondon = DateTime.Now.AddHours(4).ToLongTimeString();
+            Timer.Tick += Timer_TickForLondon;
+            Timer.Interval = 500;
+            Timer.Start();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
